0e81a0c61f8ca0bd94f36475049ab4b5b0aed7ad
Parky Stevey <parkgrrr@gmail.com>
2017-02-10 14:16:48 -0700

    First commit.  Chapter 7 completed with a crime detail fragment created

---
+++ app/src/main/java/net/parkerstevens/criminalintent/Crime.java
@@ -0,0 +1,48 @@
+package net.parkerstevens.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by pstev on 2/10/2017.
+ */
+
+public class Crime {
+    private UUID mID;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        mID = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getID() {
+        return mID;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
