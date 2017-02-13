bb3f97a8a440cd9802d062e60734d06ec9f01c03
Parky Stevey <parkgrrr@gmail.com>
2017-02-13 14:33:29 -0700

    Chapter 9 completed with logo added for crimes solved, and constraint layout used.

--- app/src/main/java/net/parkerstevens/criminalintent/CrimeListFragment.java
+++ app/src/main/java/net/parkerstevens/criminalintent/CrimeListFragment.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -52,13 +53,16 @@ public class CrimeListFragment extends Fragment {
 
         private TextView mTitleTextView;
         private TextView mDateTextView;
+        private ImageView mSolvedImage;
         private Crime mCrime;
 
         public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.list_item_crime, parent, false));
             itemView.setOnClickListener(this);
+
             mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
             mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
+            mSolvedImage = (ImageView) itemView.findViewById(R.id.crime_solved);
 
         }
 
@@ -66,6 +70,7 @@ public class CrimeListFragment extends Fragment {
             mCrime = crime;
             mTitleTextView.setText(mCrime.getTitle());
             mDateTextView.setText(mCrime.getDate().toString());
+            mSolvedImage.setVisibility((crime.isSolved()?View.VISIBLE : View.GONE));
         }
 
         @Override
